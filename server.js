4b61e23baaba0a17ab8d74178b9e5ee676f540c0
Aparna Dubey <101944716+aparna03dubey@users.noreply.github.com>
2024-03-29 01:06:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from 'path'
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -18,6 +19,9 @@ dotenv.config();
 //databse config
 connectDB();
 
+// es module fix
+const __filename=fileURLToPath(import.meta.URL);
+connst __dirname=path.dirname(__filename);
 //rest object
 const app = express();
 
